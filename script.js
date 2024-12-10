d396fe1ba3ddca841bbee464f0e0b5a157736d10
JITENDER <138762808+jeetsingh-621@users.noreply.github.com>
2024-12-10 11:22:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+document.getElementById("userForm").addEventListener("submit", function (event) {
+  event.preventDefault(); 
+  const nameInput = document.getElementById("name").value.trim();
+  const ageInput = parseInt(document.getElementById("age").value.trim());
+
+  
+  if (!nameInput || isNaN(ageInput)) {
+    alert("Both fields are required.");
+    return;
+  }
+
+  
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (ageInput > 18) {
+        resolve(`Welcome, ${nameInput}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${nameInput}. You aren't old enough.`);
+      }
+    }, 4000); 
+  })
+    .then((message) => {
+      alert(message);
+    })
+    .catch((error) => {
+      alert(error);
+    });
+});
+
